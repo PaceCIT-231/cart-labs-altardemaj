e9a31a467f58afa4cc04bb0c00a50643ddee65f8
Altardemaj <ad17603p@pace.edu>
2022-11-16 14:02:42 -0500

    Changes

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,12 +12,27 @@ function addToCart(cookie) {
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
    //add 1 to the itemCount variable
+   itemCount = itemCount+1
+    console.log(itemCount);
+
    //add the correct price to the currentPrice variable
+
+if (cookie == "peanut butter") {
+    currentPrice = currentPrice + 20
+} else if (cookie == "sandies") {
+    currentPrice = currentPrice + 30
+} else if ( cookie == "party press") {
+    currentPrice = currentPrice + 35
+} else if (cookie = "chocolate chip") {
+    currentPrice = currentPrice + 25
+}
+
+console.log(currentPrice)
 
 }
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    
+  window.alert(`You have a total of ${itemCount} items that will cost $${currentPrice}. Thank you for shopping with us`)
 }
