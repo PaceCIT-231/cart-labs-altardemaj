4c9331361c84dec977dbe2be7962f76f8380ab46
Altardemaj <ad17603p@pace.edu>
2022-12-07 13:20:40 -0500

    changes

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,19 @@
-let currentPrice=0, itemCount=0
+const cart = {
+    currentPrice: 0,
+    items: [],
+    addItem: function(cookie, price) {
+        // add a cookie (string) to the items array
+    this.items.push(cookie)
+        //add the price (number) to the currentPrice properties
+    this.currentPrice= this.currentPrice + price 
+    },
+    clear: function() {
+        //reset the currentPrice and items properties
+        this.currentPrice= 0
+        this.items= []
+    },
+}
+
 
 function addToCart(cookie) {
     /* 
@@ -8,54 +23,38 @@ function addToCart(cookie) {
         party press: 35
         chocolate chip: 25
     */
-
+   
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
+
+
    //add 1 to the itemCount variable
-   itemCount = itemCount+1
-    console.log(itemCount)
-    
-    document.getElementById("cartItems").innerHTML= itemCount
-
    //add the correct price to the currentPrice variable
-   if (cookie == 'peanut butter') {
-    currentPrice = currentPrice + 20
-    }
-    else if (cookie == 'sandies') {
-    currentPrice = currentPrice + 30
-    }
-    else if (cookie == 'party press') {
-    currentPrice = currentPrice + 35
-    }
-    else if (cookie == 'chocolate chip') {
-    currentPrice = currentPrice + 25
-    }
+   if (cookie == 'peanut butter'){cart.addItem(cookie, 20)}
+   if (cookie == 'sandies') {cart.addItem(cookie, 30)}
+   if (cookie == 'party press') {cart.addItem(cookie, 35)}
+   if (cookie == 'chocolate chip') {cart.addItem(cookie, 25)}
 
-document.querySelector(".hoverText").innerHTML = currentPrice
-
-
-console.log(currentPrice)
+   document.querySelector(".hoverText").innerHTML = cart.currentPrice
+    console.log(cart)
+    document.getElementById("cartItems").innerHTML= cart.items.length
 }
+
+
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    //window.alert(`You have a total of ${itemCount} items that will cost $${currentPrice}.  Thank you for shopping with us!`)
-   window.prompt(`Please provide your name and address \nThank you for shopping with us!`)
-
-    currentPrice = 0
-    itemCount = 0
-
-    document.getElementById("cartItems").innerHTML= itemCount
-    document.querySelector(".hoverText").innerHTML = currentPrice
-
+    window.alert(`Item Count: ${cart.items.length} Total Cost: ${cart.currentPrice}`)
+    prompt(`What is your name and address so we can bill you and send you your purchases?`)
+    cart.clear()
+    document.querySelector(".hoverText").innerHTML = cart.currentPrice
+    console.log(cart)
+    document.getElementById("cartItems").innerHTML= cart.items.length
 }
 
-function darkMode() {
-	// add your code here
-    
-    document.querySelector("body").style.backgroundColor = "black"
-    document.querySelector("main").style.color = "white"
-    document.querySelector("h1").style.color = "white"
 
+function darkMode() {
+	document.querySelector("body").style.backgroundColor="navy"
+    document.querySelector("body").style.color="white"
 }
