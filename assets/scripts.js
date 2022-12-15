5bde26b3d669508ea4282a99a0932ba90f162012
Altardemaj <ad17603p@pace.edu>
2022-12-14 21:59:32 -0500

    committ

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,16 +1,26 @@
 const cart = {
     currentPrice: 0,
     items: [],
+    
     addItem: function(cookie, price) {
         // add a cookie (string) to the items array
     this.items.push(cookie)
         //add the price (number) to the currentPrice properties
     this.currentPrice= this.currentPrice + price 
     },
+
     clear: function() {
         //reset the currentPrice and items properties
         this.currentPrice= 0
         this.items= []
+    },
+
+    getSummary: function() {
+        let summary = `<p>Number of Items: ${this.items.length}</p>
+            <h4>Details</h4>
+            <p>${this.items.join('<br>')}</p>
+            <p>Total Price: $${this.currentPrice}</p>`
+        return summary
     },
 }
 
@@ -46,7 +56,10 @@ function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
     window.alert(`Item Count: ${cart.items.length} Total Cost: ${cart.currentPrice}`)
-    prompt(`What is your name and address so we can bill you and send you your purchases?`)
+
+    document.getElementById("summary-body").innerHTML= cart.getSummary()
+    document.getElementById("summary").style.display= "block"
+    
     cart.clear()
     document.querySelector(".hoverText").innerHTML = cart.currentPrice
     console.log(cart)
@@ -58,3 +71,14 @@ function darkMode() {
 	document.querySelector("body").style.backgroundColor="navy"
     document.querySelector("body").style.color="white"
 }
+
+
+function clearCart() {
+    cart.clear()
+    document.querySelector(".hoverText").innerHTML = cart.currentPrice
+    console.log(cart)
+    document.getElementById("cartItems").innerHTML= cart.items.length
+}
+
+clearcart.addEventListener("mousedown", clearCart)
+//the first clearcart above is the id of the clear cart button in index.html
